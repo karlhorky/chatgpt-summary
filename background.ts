8e56cdd8a750e55c4858e6d05ff2eb8dc7fec67d
Karl Horky <karl.horky@gmail.com>
2024-08-17 16:42:40 +0200

    Add extension click functionality

--- background.ts
+++ background.ts
@@ -1,1 +1,13 @@
-console.log('Hello from the background script!')
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.id) {
+    const prompt = encodeURIComponent(
+      `Summarize ${tab.url} in 5 bullet points`,
+    );
+
+    await chrome.tabs.create({
+      // https://webapps.stackexchange.com/a/175084/29415
+      url: `https://chat.openai.com/?q=${prompt}`,
+      index: tab.index + 1,
+    });
+  }
+});
